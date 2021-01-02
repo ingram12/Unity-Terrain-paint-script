07b6e858d0df09a7507a3d2961e7b1325867d5f9
ingram12 <ingram_it@bk.ru>
2021-01-02 20:07:24 +0300

    init

---
+++ TerrainPaint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainPaint : MonoBehaviour
+{
+    public float scale = 50;
+    public int xOffset = 1;
+    public int yOffset = 1;
+    public float coef = 1;
+    private float[,,] alphaData;
+    private TerrainData tData;
+
+    public void Paint() {
+        tData = Terrain.activeTerrain.terrainData;
+        alphaData = tData.GetAlphamaps(0, 0, tData.alphamapWidth, tData.alphamapHeight);
+
+        for (int y = 0; y < tData.alphamapHeight; y++){
+            for (int x = 0; x < tData.alphamapWidth; x++){
+                float kx = (((float)x * xOffset) / tData.alphamapWidth) * scale;
+                float ky = (((float)y * yOffset) / tData.alphamapHeight) * scale;
+                float ty = Mathf.PerlinNoise(kx, ky) * coef;
+
+                //float tr = Mathf.PerlinNoise(kx * 8, ky * 8) * coef;
+                //tr = tr > 0.5f ? 0.5f : tr;
+                //ty = ty < tr ? tr : ty;
+
+                alphaData[x, y, 0] = 1 - ty;
+                alphaData[x, y, 1] = ty;
+            }
+        }
+       
+        tData.SetAlphamaps(0, 0, alphaData);
+    }
+}
+
---
+++ TerrainPaintButton.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(TerrainPaint))]
+public class TerrainPaintButton : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        TerrainPaint myScript = (TerrainPaint)target;
+ 
+        if (GUILayout.Button("Paint"))
+        {
+            myScript.Paint();
+        }
+    }
+}
